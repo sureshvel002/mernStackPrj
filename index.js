ab45c14d98c549b454102e6b5f609a1d00ab40e4
sureshvel002 <sureshvel2002@gmail.com>
2023-12-31 23:06:31 +0530

    newyear 24

--- index.js
+++ index.js
@@ -9,20 +9,18 @@ app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 app.use('/todos',todoRoute);
-const mongodb = "mongodb+srv://sureshvdm02:sureshvdm@cluster0.hiamz3a.mongodb.net/todo-appdb?retryWrites=true&w=majority";
+const mongodb = "mongodb+srv://sureshvdm02:sureshvdm@cluster0.hiamz3a.mongodb.net/NewYear?retryWrites=true&w=majority";
 app.get('/', (req, res) => {
     res.send('Welcome to smart server')
 })
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 // mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
 //     app.listen(3000);
 //     console.log(`server is running on port 3000`)
 // }).catch(err => console.log(err))
 // const PORT = process.env.PORT || 5000;
-
-// mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}).then(() => {
-//     app.listen(5000,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
-
+mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}).then(() => {
+    app.listen(5000,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
 //deploy server
-mongoose.connect(process.env.MDB, { useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
-    app.listen(8080,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
+// mongoose.connect(process.env.mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}).then(() => {
+//     app.listen(5000,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
