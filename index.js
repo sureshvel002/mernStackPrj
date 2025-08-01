1872c3aee3fb801dbbdb392dc01b66fff882d1d1
sureshvel002 <sureshvel2002@gmail.com>
2025-08-01 21:58:27 +0000

    update mongoose version

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import todoRoute from './routes/todoroute.js';
+
 const app = express();
 dotenv.config();
-app.use(express.json({ extended: true }))
-app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/todos',todoRoute);
+
+app.use('/todos', todoRoute);
+
 const mongodb = "mongodb+srv://sureshvdm02:sureshvdm@cluster0.hiamz3a.mongodb.net/todo-appdb?retryWrites=true&w=majority";
+
 app.get('/', (req, res) => {
-    res.send('Welcome to smart server')
-})
+    res.send('Welcome to smart server');
+});
+
 const PORT = process.env.PORT || 8080;
-// mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
-//     app.listen(3000);
-//     console.log(`server is running on port 3000`)
-// }).catch(err => console.log(err))
-// const PORT = process.env.PORT || 5000;
 
-mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}).then(() => {
-    app.listen(5000,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
+// Set strictQuery to suppress the deprecation warning
+mongoose.set('strictQuery', true);
 
-//deploy server
-// mongoose.connect(process.env.MDB, { useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
-//     app.listen(8080,console.log(`server is running on port ${PORT}`))}).catch(err => console.log(err))
+// Connect to MongoDB without the deprecated options
+mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}).catch(err => console.log(err));
